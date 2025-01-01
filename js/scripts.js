e720fc36c356f9af2b970629b769afdbc8d7459d
Dan <pulberdan@gmail.com>
2025-01-01 11:59:57 -0500

    feat: add categories functionality

--- js/scripts.js
+++ js/scripts.js
@@ -3,17 +3,19 @@
  */
 
 // Fetch products from the API and display them on the page
-document.addEventListener('DOMContentLoaded', () => {
-    fetchProducts();
-});
+document.addEventListener('DOMContentLoaded', async () => {
+    await fetchCategories()
+    setActiveCategory('all')
+})
 
 // Fetch products from the API
 function fetchProducts() {
+    setProductsLoading()
     fetch('https://fakestoreapi.com/products')
         .then(response => response.json())
         .then(data => {
             displayProducts(data);
-            displayCategories(getAllCategories(data));
+            endProductsLoading()
         })
         .catch(error => console.error('Error fetching products:', error));
 }
@@ -41,3 +43,75 @@ function createProductElement(product) {
     return productElement;
 }
 
+// Display Loading Spinner
+function setProductsLoading() {
+    const productGrid = document.querySelector('.product-grid')
+    const productsLoader = document.querySelector('.products-loader')
+    productGrid.style.display = 'none'
+    productsLoader.style.display = 'flex'
+}
+// Hide Loading Spinner
+function endProductsLoading() {
+    const productGrid = document.querySelector('.product-grid')
+    const productsLoader = document.querySelector('.products-loader')
+    productGrid.style.display = 'grid'
+    productsLoader.style.display = 'none'
+}
+
+
+// Update UI to selected category
+function setActiveCategory(category) {
+    fetchProductsFromCategory(category)
+
+    const categoryElements = document.querySelectorAll(`.category`)
+    categoryElements.forEach(element => element.classList.remove('selected-category'))
+
+    const categoryElement = document.querySelector(`.category[data-category="${category}"]`)
+    if (categoryElement) categoryElement.classList.add('selected-category')
+}
+
+// Fetch categories from the API
+async function fetchCategories() {
+    await fetch('https://fakestoreapi.com/products/categories')
+        .then(response => response.json())
+        .then(data => {
+            displayCategories(data)
+        })
+        .catch(error => console.error('Error fetching categories:', error))
+}
+
+// Fetch products for category from the API
+function fetchProductsFromCategory(category) {
+    if (category === 'all') fetchProducts()
+    else {
+        setProductsLoading()
+        fetch(`https://fakestoreapi.com/products/category/${category}`)
+            .then(response => response.json())
+            .then(data => {
+                displayProducts(data)
+                endProductsLoading()
+            })
+            .catch(error => console.error('Error fetching products:', error))
+    }
+}
+
+// Display category buttons
+function displayCategories(categories) {
+    categories.unshift('all')
+    const categoriesContainer = document.querySelector('.categories')
+    categoriesContainer.innerHTML = ''
+    categories.forEach(category => {
+        const categoryElement = createCategoryElement(category)
+        categoriesContainer.appendChild(categoryElement)
+        categoryElement.addEventListener('click', () => setActiveCategory(category))
+    })
+}
+
+// Function to build each category element
+function createCategoryElement(category) {
+    const categoryElement = document.createElement('button')
+    categoryElement.classList.add('category')
+    categoryElement.innerText = category
+    categoryElement.setAttribute('data-category', category)
+    return categoryElement
+}
