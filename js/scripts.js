be4d5bb0f0c4da89e908063d6c215a62d1443b0f
Camron2889 <Camron2889@gmail.com>
2025-01-02 03:38:13 -0500

    Replace single- with double-quotes

--- js/scripts.js
+++ js/scripts.js
@@ -3,22 +3,22 @@
  */
 
 // Fetch products from the API and display them on the page
-document.addEventListener('DOMContentLoaded', async () => {
     [products] = await Promise.all([fetchProducts()]);
+document.addEventListener("DOMContentLoaded", async () => {
     displayProducts(products);
 });
 
 // Fetch products from the API
 async function fetchProducts() {
-    return fetch('https://fakestoreapi.com/products')
+    return fetch("https://fakestoreapi.com/products")
         .then(response => response.json())
         .catch(error => console.error('Error fetching products:', error));
 }
 
 // Display all products on the page based on the given data
 function displayProducts(products) {
-    const productGrid = document.querySelector('.product-grid');
-    productGrid.innerHTML = '';
+    const productGrid = document.querySelector(".product-grid");
+    productGrid.innerHTML = "";
     products.forEach(product => {
         const productElement = createProductElement(product);
         productGrid.appendChild(productElement);
@@ -27,8 +27,8 @@ function displayProducts(products) {
 
 // Function to build each product card element
 function createProductElement(product) {
-    const productElement = document.createElement('article');
-    productElement.classList.add('product');
+    const productElement = document.createElement("article");
+    productElement.classList.add("product");
     productElement.innerHTML = `
         <img src="${product.image}" alt="${product.title}" class="product-image">
         <h2 class="title">${product.title}</h2>
