08fd379914cdba654e7db9f39d55b706aef0dca6
Camron2889 <Camron2889@gmail.com>
2025-01-04 06:33:21 -0500

    Fix indent, add missing setters and getters

--- take-home/src/stores/products.js
+++ take-home/src/stores/products.js
@@ -2,33 +2,45 @@ import { defineStore } from 'pinia'
 
 export const useProductStore = defineStore('productStore', {
   state: () => ({
-      products: [],
-      filterContains: ""
+    products: [],
+    filterContains: "",
+    filterCategory: ""
   }),
   getters: {
-      allProducts: (state) => {
-          if (state.filterContains) {
-              return state.products.filter(
-                  product => product.title.toLowerCase().includes(state.filterContains.toLowerCase())
-              );
-          }
-          return state.products;
-      },
-      filteredProducts: (state) => {
-          if (state.filterContains) {
-              return state.products.filter(
-                  product => product.title.toLowerCase().includes(state.filterContains.toLowerCase())
-              );
-          }
-          return state.products;
+    allProducts: (state) => {
+      return state.products;
+    },
+    filteredProducts: (state) => {
+      let copy = state.products;
+      if (state.filterCategory) {
+        copy = copy.filter(
+          product => product.category === state.filterCategory
+        );
       }
+      if (state.filterContains) {
+        copy = copy.filter(
+          product => product.title.toLowerCase().includes(state.filterContains.toLowerCase())
+        )
+      }
+      
+      return copy;
+    },
+    category: (state) => {
+      return state.filterCategory;
+    },
+    contains: (state) => {
+      return state.filterContains;
+    }
   },
   actions: {
-      setProducts(products) {
-          this.products = products;
-      },
-      setFilterContains(filter) {
-          this.filterContains = filter;
-      }
+    setProducts(products) {
+      this.products = products;
+    },
+    setFilterCategory(category) {
+      this.filterCategory = category;
+    },
+    setFilterContains(contains) {
+      this.filterContains = contains;
+    }
   }
 });
